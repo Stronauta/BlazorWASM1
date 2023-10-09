50fb061cc64049ec7263b61825dfa514912af491
Stronauta <samircastillop@gmail.com>
2023-10-09 18:49:00 -0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+using Blazor_WEA_Samir.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Blazor_WEA_Samir.Client
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
+
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
